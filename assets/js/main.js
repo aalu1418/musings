e035af73bbeb356187d759be6ab4facf60c69e76
aalu1418 <aalu1418@gmail.com>
2020-03-29 18:50:13 -0400

    scroll fading for front page

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,4 +10,16 @@ $(".excerpt > p").click(event => {
 
 $(document).ready(() => {
   $("h1, .subtitle").css("opacity", "1")
+  const frontHeight = $(".frontPage").outerHeight(true) * 0.8;
+
+  //fade in/out front page depending on scroll position
+  $(window).scroll(() => {
+    const space = frontHeight - $(window).scrollTop()
+    if ( space > 0) {
+      $(".rain-container").css("opacity", space/frontHeight);
+    } else {
+      $(".rain-container").css("opacity", 0)
+    }
+  })
+
 })
